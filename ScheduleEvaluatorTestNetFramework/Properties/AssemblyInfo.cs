a75b3f69da7ab37ccef0939a0987e5a526d946d3
CJ Windows <cjhillbrand@gmail.com>
2020-04-27 10:55:30 -0700

    Changed Target Framework of Unit tests

---
+++ ScheduleEvaluatorTestNetFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ScheduleEvaluatorTestNetFramework")]
+[assembly: AssemblyDescription("Data Source = 65.175.68.34; Initial Catalog = vsaDev; Persist Security Info = True; User ID = sa; Password = kD$wg&OUrhfC6AMMq6q5Xh"j)]
+[assembly: AssemblyConfiguration("Data Source = 65.175.68.34; Initial Catalog = vsaDev; Persist Security Info = True; User ID = sa; Password = kD$wg&OUrhfC6AMMq6q5Xh"j)]
+[assembly: AssemblyCompany("Data Source = 65.175.68.34; Initial Catalog = vsaDev; Persist Security Info = True; User ID = sa; Password = kD$wg&OUrhfC6AMMq6q5Xh"j)]
+[assembly: AssemblyProduct("ScheduleEvaluatorTestNetFramework")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("Data Source = 65.175.68.34; Initial Catalog = vsaDev; Persist Security Info = True; User ID = sa; Password = kD$wg&OUrhfC6AMMq6q5Xh"j)]
+[assembly: AssemblyCulture("Data Source = 65.175.68.34; Initial Catalog = vsaDev; Persist Security Info = True; User ID = sa; Password = kD$wg&OUrhfC6AMMq6q5Xh"j)]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cdcb15f5-3e04-4cc4-89ea-1c9dfb6bfec3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
