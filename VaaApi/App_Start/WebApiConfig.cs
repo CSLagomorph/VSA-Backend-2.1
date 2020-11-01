4219ea7d035a59e4d747f493539e432a0cd50cb8
jadonc <46636295+jadonc@users.noreply.github.com>
2020-11-01 00:48:37 -0700

    Added descriptions/documentation

--- VaaApi/App_Start/WebApiConfig.cs
+++ VaaApi/App_Start/WebApiConfig.cs
@@ -13,9 +13,22 @@ namespace VaaApi
         {
             // Web API configuration and services
             config.EnableCors();
+            
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            // "routeTemplate":
+            //      the structure of allowed HTTP Request strings that get concatenated to the host domain name;
+            //      for example --> if the VAA software runs on "my.server.com", then an HTTP GET or POST request
+            //      sent to the following sample paths (both valid) will be recognized by our [EnableCors]-enabled web API controllers:
+            //          - "my.server.com/api/SomeController/9999"
+            //          - "my.server.com/api/Another_controller"
+            //
+            // "defaults":
+            //      you can specify default parameters, even ones that don't appear in the <routeTemplate>;
+            //      parameters that DON'T appear in routeTemplate (as far as i know) can't be changed from their default value(s);
+            //      for example --> this is valid:
+            //          - defaults: new { invisible_param = "Spooky", id = RouteParameter.Optional }
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
